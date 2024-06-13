e8e20538d18d6ba804be1c812419d5658b1b3eb6
Timi Olanrewaju <timi.olanrewaju@cloud.ziffdavis.com>
2024-06-13 15:09:46 -0500

    Day 4 updated Code

--- Beginner/Rock Paper Scissors/main.py
+++ Beginner/Rock Paper Scissors/main.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -25,9 +27,44 @@ scissors = '''
 ---.__(___)
 '''
 
-#make choices defined variables
-rock = 0 
-paper = 1
-scissors = 2
 
-user = input("What do you choose? Type 0 for Rock, 1 for Paper")
+user = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+cpu = random.randint(0,2)
+
+if user == 0: 
+  print(rock+"\n")
+  print("Computer chose:\n")
+  if cpu == 0:
+    print(rock+"\n")
+    print("You tie.")
+  elif cpu == 1:
+    print(paper+"\n")
+    print("You lose.")
+  else:
+    print(scissors+"\n")
+    print("You win.")
+elif user == 1:
+  print(paper+"\n")
+  print("Computer chose:\n")
+  if cpu == 0:
+    print(rock+"\n")
+    print("You win.")
+  elif cpu == 1:
+    print(paper+"\n")
+    print("You tie.")
+  else:
+    print(scissors+"\n")
+    print("You lose.")
+else:
+  print(scissors+"\n")
+  print("Computer chose:\n")
+  if cpu == 0:
+    print(rock+"\n")
+    print("You lose.")
+  elif cpu == 1:
+    print(paper+"\n")
+    print("You win.")
+  else:
+    print(scissors+"\n")
+    print("You tie.")
+  
