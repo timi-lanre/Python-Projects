589fb409f2f9e4dc7ac1bf81811ffd798eac1ffe
Timi Olanrewaju <timi.olanrewaju@cloud.ziffdavis.com>
2024-08-05 18:02:59 -0500

    Updating User experience

--- Beginner/Hangman/main.py
+++ Beginner/Hangman/main.py
@@ -26,23 +26,28 @@ placeholder = "_" * len_guessed
 
 
 end_check = '_'
-
+guess_tracker = ''
 
 index = 0 
 while guess_limit > 0:
   guess = input("Guess a letter:\n").lower()
   if guess in guessed_word: 
+    if guess in guess_tracker:
+        print("You already guessed this, try again.")
     pos_x = charposition(guessed_word, guess)
     for i in pos_x:
         placeholder = placeholder[:i] + guess + placeholder[i+1:]
-        print(placeholder)
   else:
+    if guess in guess_tracker:
+        print("You already guessed this, try again.")
+        guess_limit += 1
     guess_limit -= 1
     print(stages[guess_limit])
-    print(placeholder)
+  print(placeholder)
   if end_check not in placeholder:
            print("You Win")
            exit()
+  guess_tracker = guess_tracker + guess
 
 if guess_limit == 0: 
   print("You Lose")
